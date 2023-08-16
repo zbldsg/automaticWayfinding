c8b2c06d664eb04fa3cda2ad9c8f316959a2fffe
zbl <zbldsg@163.com>
2023-08-16 14:15:10 +0800

    加入下拉框

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,8 @@ import AStar from "javascript-astar";
 import {gsap} from "gsap";
 
 import {onWindowResize} from "./Common/resize";
+import {deleteGroupFromScene} from "./Common/clean";
 import {initAll} from "./Init/init";
-import {generateFloor} from "./Generate/floor";
 
 const renderers = {
   renderer: null,
@@ -34,9 +34,14 @@ const baseSceneData = {
   sceneObject: null,
 };
 
+let cube,line,boxGroup = new THREE.Group();
+
 const menu = ref({
   display: true,
 });
+
+
+
 
 onMounted(() => {
   dom = document.querySelector("#test");
@@ -55,7 +60,10 @@ function init() {
   // initGrid();
   // initLine();
   // initBox();
-  initT();
+  // initT();
+
+  //初始化网格
+  generateLine()
   animate();
 }
 
@@ -72,7 +80,7 @@ function render() {
 }
 
 
-function initT() {
+function initT(row,col) {
   //length必须是偶数
   // let grid = [
   //   [1, 0, 1, 0, 1, 1],
@@ -81,16 +89,18 @@ function initT() {
   //   [1, 1, 1, 1, 1, 1]
   // ];
 
+  // let row = 20;
+  // let col = 22;
   //随机生成2维数组
-  let grid = generateRandomGrid(20,22)
+  let grid = generateRandomGrid(row, col)
+  console.log(grid,'grid')
 
   // 执行A*搜索
-  const result = astarSearch(grid,19,21);
+  const result = astarSearch(grid, row - 1, col - 1);
+  console.log(result,'result')
 
-  //初始化网格
-  generateLine()
   //生成box
-  map(grid)
+  generateBox(grid)
   //生成路线
   generateRoad(grid, result)
 }
@@ -100,7 +110,7 @@ function generateLine() {
   current.scene.add(gridHelper);
 }
 
-function astarSearch(grid,x,y) {
+function astarSearch(grid, x, y) {
   const graph = new AStar.Graph(grid, {diagonal: false});
 
   // 使用 A* 算法寻找路径
@@ -117,8 +127,11 @@ function generateRoad(grid, result) {
   //总宽度 z轴 二维数组的外层数组
   const width = grid.length;
 
-  let xNum = .5
-  let zNum = -1.5
+  let xNum = -.5
+  let zNum = -.5
+
+  // let xNum = .5
+  // let zNum = -1.5
 
   let arr = []
   for (let i = 0; i < result.length; i++) {
@@ -137,7 +150,7 @@ function generateRoad(grid, result) {
   anima(arr)
 }
 
-function map(grid) {
+function generateBox(grid) {
   const one = grid.length;
   const two = grid[0].length;
 
@@ -161,15 +174,15 @@ function map(grid) {
       //y 为二维数组外层长度 减去外层长度的一半(移动到中心)
       matrix.makeTranslation(j - two / 2 + 0.5, .5, i - one / 2 + 0.5);
       cube.applyMatrix4(matrix);
-      current.scene.add(cube);
+      boxGroup.add(cube)
 
       const line = new THREE.LineSegments(edges, lineMaterial);
       line.position.set(cube.position.x, cube.position.y, cube.position.z);
-      current.scene.add(line);
-
-
+      boxGroup.add(line);
     }
   }
+
+  current.scene.add(boxGroup);
 
 // 循环遍历二维数组，创建 Three.js 立方体表示地图
 //   for (let i = 0; i < two; i++) {
@@ -194,7 +207,7 @@ function map(grid) {
 //   }
 }
 
-function generateRandomGrid(row,col) {
+function generateRandomGrid(row, col) {
   const grid = [];
   for (let i = 0; i < row; i++) {
     const row = [];
@@ -208,20 +221,25 @@ function generateRandomGrid(row,col) {
 }
 
 function anima(arr) {
-  const box = new THREE.BoxGeometry(1, 1, 1);
-  const black = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: true, opacity: .8});
-  const cube = new THREE.Mesh(box, black);
+  if(!cube) {
+    const box = new THREE.BoxGeometry(1, 1, 1);
+    const black = new THREE.MeshBasicMaterial({color: 0xff0000, transparent: true, opacity: .8});
+    cube = new THREE.Mesh(box, black);
 
-  cube.position.set(arr[0].x, .5, arr[0].z)
-  current.scene.add(cube)
+    cube.position.set(arr[0].x, .5, arr[0].z)
+    current.scene.add(cube)
+  } else {
+    cube.position.set(arr[0].x, .5, arr[0].z)
+  }
+
 
   let count = 0;
 
   gsap.to(cube.position, {
     duration: 5,
-    onUpdate:()=> {
-      if(Math.random() < 0.2) {
-        if(count < arr.length) {
+    onUpdate: () => {
+      if (Math.random() < 0.2) {
+        if (count < arr.length) {
           cube.position.set(arr[count].x, .5, arr[count].z);
           count++
         }
@@ -230,114 +248,142 @@ function anima(arr) {
   });
 }
 
-function generateBox(grid) {
-  const m = grid.length;
-  const n = grid[0].length;
+// function generateBox(grid) {
+//   const m = grid.length;
+//   const n = grid[0].length;
+//
+//   let box = new THREE.BoxGeometry(1, 1, 1);
+//   let blackMaterial = new THREE.MeshBasicMaterial({color: 0x000000, transparent: true, opacity: .8});
+//   let grayMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, transparent: true, opacity: .1});
+//   let group = new THREE.Group();
+//
+//   //总长度 x轴, 10个格子,二维数组的里层数组
+//   const length = n;
+//   //总宽度 z轴 8个格子,二维数组的外层数组
+//   const width = m;
+//
+//   // 每隔几个格子生成一个方块
+//   const number = 1;
+//
+//   for (let i = 0; i < width / number; i++) {
+//     for (let j = 0; j < length / number; j++) {
+//       let cube = new THREE.Mesh(box, grid[i][j] === 0 ? blackMaterial : grayMaterial);
+//       //这里number * j 是随着j的增大,不断往X正轴前移
+//       // - length/2 是为了让所有正方体正好落在x,y 轴的中心, 而不是右上角(因为x,y都是正的)
+//       /**
+//        * 这里number * j 是随着j的增大,不断往X正轴前移
+//        * - length/2 是为了让所有正方体正好落在x,y 轴的中心, 而不是右上角(因为x,y都是正的)
+//        * 这里x和y 都要加上0.5 也就是box大小的一半, 防止box放在4个格子中间
+//        */
+//       let x = number * j - length / 2 + .5;
+//       let z = number * i - width / 2 + .5;
+//       cube.position.set(x, 0.5, z);
+//       group.add(cube);
+//
+//       const edges = new THREE.EdgesGeometry(box);
+//       const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: 0xffffff}));
+//       line.position.set(x, 0.5, z);
+//       group.add(line);
+//     }
+//   }
+//
+//   current.scene.add(group)
+// }
+//
+// function initGrid() {
+//   // 创建一个图,二维数组
+//   const graph = new AStar.Graph([
+//     [1, 1, 1, 1, 0],
+//     [1, 0, 0, 1, 0],
+//     [1, 1, 1, 1, 0],
+//     [0, 0, 0, 1, 1]
+//   ], {diagonal: false});
+//
+//   // 使用 A* 算法寻找路径
+//   const startNode = graph.grid[0][0];
+//   const endNode = graph.grid[3][4];
+//
+//   // 执行A*搜索
+//   const result = AStar.astar.search(graph, startNode, endNode);
+//   console.log(result, '我醉了')
+//
+// }
+//
+// function initBox() {
+//   let box = new THREE.BoxGeometry(1, 1, 1);
+//   let grayMaterial = new THREE.MeshBasicMaterial({color: 0xC0C0C0});
+//   let redMaterial = new THREE.MeshBasicMaterial({color: 0xC00000});
+//   let group = new THREE.Group();
+//
+//   //总长度 x轴, 10个格子
+//   const length = 10;
+//   //总宽度 z轴 8个格子
+//   const width = 8;
+//
+//   // 每隔几个格子生成一个方块
+//   const number = 1;
+//
+//   for (let i = 0; i < width / number; i++) {
+//     for (let j = 0; j < length / number; j++) {
+//       let cube = null;
+//       if (j % 2 > 0) {
+//         cube = new THREE.Mesh(box, grayMaterial);
+//       } else {
+//         cube = new THREE.Mesh(box, redMaterial);
+//       }
+//
+//       // if (salt <= randomNumber) {
+//       // let cube = new THREE.Mesh(box, material);
+//       //这里number * j 是随着j的增大,不断往X正轴前移
+//       // - length/2 是为了让所有正方体正好落在x,y 轴的中心, 而不是右上角(因为x,y都是正的)
+//       /**
+//        * 这里number * j 是随着j的增大,不断往X正轴前移
+//        * - length/2 是为了让所有正方体正好落在x,y 轴的中心, 而不是右上角(因为x,y都是正的)
+//        * 这里x和y 都要加上0.5 也就是box大小的一半, 防止box放在4个格子中间
+//        */
+//       let x = number * j - length / 2 + .5;
+//       let z = number * i - width / 2 + .5;
+//       cube.position.set(x, 0.5, z);
+//       group.add(cube);
+//       // }
+//
+//     }
+//   }
+//
+//   current.scene.add(group)
+// }
 
-  let box = new THREE.BoxGeometry(1, 1, 1);
-  let blackMaterial = new THREE.MeshBasicMaterial({color: 0x000000, transparent: true, opacity: .8});
-  let grayMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, transparent: true, opacity: .1});
-  let group = new THREE.Group();
-
-  //总长度 x轴, 10个格子,二维数组的里层数组
-  const length = n;
-  //总宽度 z轴 8个格子,二维数组的外层数组
-  const width = m;
-
-  // 每隔几个格子生成一个方块
-  const number = 1;
-
-  for (let i = 0; i < width / number; i++) {
-    for (let j = 0; j < length / number; j++) {
-      let cube = new THREE.Mesh(box, grid[i][j] === 0 ? blackMaterial : grayMaterial);
-      //这里number * j 是随着j的增大,不断往X正轴前移
-      // - length/2 是为了让所有正方体正好落在x,y 轴的中心, 而不是右上角(因为x,y都是正的)
-      /**
-       * 这里number * j 是随着j的增大,不断往X正轴前移
-       * - length/2 是为了让所有正方体正好落在x,y 轴的中心, 而不是右上角(因为x,y都是正的)
-       * 这里x和y 都要加上0.5 也就是box大小的一半, 防止box放在4个格子中间
-       */
-      let x = number * j - length / 2 + .5;
-      let z = number * i - width / 2 + .5;
-      cube.position.set(x, 0.5, z);
-      group.add(cube);
-
-      const edges = new THREE.EdgesGeometry(box);
-      const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: 0xffffff}));
-      line.position.set(x, 0.5, z);
-      group.add(line);
-    }
-  }
-
-  current.scene.add(group)
+function generate() {
+  console.log(row.value, 'generate')
+  deleteGroupFromScene(current.scene,boxGroup)
+  deleteGroupFromScene(current.scene,cube)
+  deleteGroupFromScene(current.scene,line)
+  initT(row.value,row.value)
 }
 
-function initGrid() {
-  // 创建一个图,二维数组
-  const graph = new AStar.Graph([
-    [1, 1, 1, 1, 0],
-    [1, 0, 0, 1, 0],
-    [1, 1, 1, 1, 0],
-    [0, 0, 0, 1, 1]
-  ], {diagonal: false});
+const row = ref(10)
 
-  // 使用 A* 算法寻找路径
-  const startNode = graph.grid[0][0];
-  const endNode = graph.grid[3][4];
-
-  // 执行A*搜索
-  const result = AStar.astar.search(graph, startNode, endNode);
-  console.log(result, '我醉了')
-
-}
-
-function initBox() {
-  let box = new THREE.BoxGeometry(1, 1, 1);
-  let grayMaterial = new THREE.MeshBasicMaterial({color: 0xC0C0C0});
-  let redMaterial = new THREE.MeshBasicMaterial({color: 0xC00000});
-  let group = new THREE.Group();
-
-  //总长度 x轴, 10个格子
-  const length = 10;
-  //总宽度 z轴 8个格子
-  const width = 8;
-
-  // 每隔几个格子生成一个方块
-  const number = 1;
-
-  for (let i = 0; i < width / number; i++) {
-    for (let j = 0; j < length / number; j++) {
-      let cube = null;
-      if (j % 2 > 0) {
-        cube = new THREE.Mesh(box, grayMaterial);
-      } else {
-        cube = new THREE.Mesh(box, redMaterial);
-      }
-
-      // if (salt <= randomNumber) {
-      // let cube = new THREE.Mesh(box, material);
-      //这里number * j 是随着j的增大,不断往X正轴前移
-      // - length/2 是为了让所有正方体正好落在x,y 轴的中心, 而不是右上角(因为x,y都是正的)
-      /**
-       * 这里number * j 是随着j的增大,不断往X正轴前移
-       * - length/2 是为了让所有正方体正好落在x,y 轴的中心, 而不是右上角(因为x,y都是正的)
-       * 这里x和y 都要加上0.5 也就是box大小的一半, 防止box放在4个格子中间
-       */
-      let x = number * j - length / 2 + .5;
-      let z = number * i - width / 2 + .5;
-      cube.position.set(x, 0.5, z);
-      group.add(cube);
-      // }
-
-    }
-  }
-
-  current.scene.add(group)
-}
+const numbers = ref([
+  {name:'10*10',value:10},
+  {name:'20*20',value:20},
+  {name:'30*30',value:30},
+  {name:'40*40',value:40},
+])
 </script>
 
 <template>
-  <div class="test" id="test"></div>
+  <div class="test" id="test">
+    <div class="menu">
+      <button @click="generate">生成地图</button>
+      <div>
+        <label for="rowSelect">地图大小：</label>
+        <select id="rowSelect" v-model="row">
+          <option v-for="num in numbers" :key="num" :value="num.value">{{ num.name }}</option>
+        </select>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <style lang="less">
@@ -347,5 +393,14 @@ function initBox() {
   overflow: hidden;
   background-color: #070a2b;
   background-size: 100%;
+
+  .menu {
+    z-index: 200;
+    position: absolute;
+    padding-left: 20px;
+    height: 80px;
+    width: 150px;
+    background-color: white;
+  }
 }
 </style>
